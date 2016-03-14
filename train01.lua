250fbd8a34e150499500740e2aef10d866997598
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-03-14 18:07:55 +0100

    added support for train on multiple input files at the same time

--- train01.lua
+++ train01.lua
@@ -60,29 +60,62 @@ progressBarSteps = math.floor(progressBarSteps / batchSize) * batchSize
 
 ----------------------------------------------------------------------
 
-function loadDataset(fname, size)
+function loadDataset(fnames, size)
 
-  loaded = torch.load(fname,'binary')
+  local data = nil
 
-  if size ~= nil and size < 1 then
-    size = math.floor(size * loaded.y:size()[1] + 0.5)
-  else
-    size = size or loaded.y:size()[1]
-    size = math.min(size, loaded.y:size()[1])
-  end
-  
-  data = {
-     data   = loaded.X,
-  
-     -- labels are 0/1 because we use cross-entropy loss
-     labels = loaded.y,
-  
-     weights = loaded.weight,
+  local totsize = 0
 
-     mvaid = loaded.mvaid,
+  -- load all input files
+  for i = 1, #fnames do
+
+    local loaded = torch.load(fnames[i],'binary')
+
+    local thisSize
+
+    -- determine the size
+    if size ~= nil and size < 1 then
+      thisSize = math.floor(size * loaded.y:size()[1] + 0.5)
+    else
+      thisSize = size or loaded.y:size()[1]
+      thisSize = math.min(thisSize, loaded.y:size()[1])
+    end
+
+    totsize = totsize + thisSize
+
+    if data == nil then
+      -- create the first entry
+
+      data = {
+         data   = loaded.X:sub(1, thisSize),
+      
+         -- labels are 0/1 because we use cross-entropy loss
+         labels = loaded.y:sub(1, thisSize),
+      
+         weights = loaded.weight:sub(1, thisSize),
+    
+         mvaid = loaded.mvaid:sub(1, thisSize),
+      }
+
+    else
+      -- append
+      data.data    = data.data:cat(loaded.X:sub(1, thisSize), 1)
+      
+      data.labels  = data.labels:cat(loaded.y:sub(1, thisSize), 1)
+
+      data.weights = data.weights:cat(loaded.weight:sub(1, thisSize), 1)
+
+      data.mvaid   = data.mvaid:cat(loaded.mvaid:sub(1, thisSize), 1)
+      
+    end
+
+
+  end -- loop over files
+
+
+  data.size = function() return totsize end
   
-     size = function() return size end
-  }
+  assert (totsize == data.data:size()[1])
 
   -- normalize weights to have an average
   -- of one per sample
@@ -93,7 +126,7 @@ function loadDataset(fname, size)
   -- DEBUG: fix weights to one
   --  data.weights = torch.Tensor(data.weights:size()[1]):fill(1):float()
  
-  return data, size
+  return data, totsize
 
 end -- function loadDataset
 
@@ -125,8 +158,8 @@ print 'loading dataset'
 -- Note: the data, in X, is 3-d: the 1st dim indexes the samples
 -- and the last two dims index the width and height of the samples.
 
-trainData, trsize = loadDataset(train_file, trsize)
-testData,  tesize = loadDataset(test_file, tesize)
+trainData, trsize = loadDataset(train_files, trsize)
+testData,  tesize = loadDataset(test_files, tesize)
 
 ----------
 -- open log file
