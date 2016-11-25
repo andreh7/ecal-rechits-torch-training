6b57dd964190fc37d61fd42810d1aa166cba2160
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-11-25 10:11:08 +0100

    added a class ResultDirData to keep data like weights and labels common to all training epochs

--- plotROCs.py
+++ plotROCs.py
@@ -18,6 +18,78 @@ import numpy as np
 
 #----------------------------------------------------------------------
 
+class ResultDirData:
+    # keeps data which is common for the entire result directory
+    def __init__(self, inputDir):
+        self.inputDir = inputDir
+
+        self.description = readDescription(inputDir)
+
+        # we don't have this for older trainings
+        self.trainWeightsBeforePtEtaReweighting = None
+
+        # check for dedicated weights and labels file
+        # train dataset
+        fname = os.path.join(inputDir, "weights-labels-train.npz")
+
+        if os.path.exists(fname):
+            data = np.load(fname)
+            self.trainWeights = data['weight']
+            self.trainWeightsBeforePtEtaReweighting = data['weightBeforePtEtaReweighting']
+            self.trainLabels = data['label']
+        else:
+            # try the BDT file (but we don't have weights before eta/pt reweighting there)
+            fname = os.path.join(inputDir, "roc-data-%s-mva.npz" % "train")
+            data = np.load(fname)
+            self.trainWeights = data['weight']
+            self.trainLabels = data['label']
+            self.trainWeightsBeforePtEtaReweighting = None
+            
+        #----------
+        # test dataset
+        #----------
+
+        fname = os.path.join(inputDir, "weights-labels-test.npz")
+        if os.path.exists(fname):
+            data = np.load(fname)
+            self.testWeights = data['weight']
+            self.testLabels = data['label']
+        else:
+            # try the BDT file
+            fname = inputDir, "roc-data-%s-mva.npz" % "test"
+            data = np.load(fname)
+            self.testWeights = data['weight']
+            self.testLabels = data['label']
+
+    #----------------------------------------
+
+    def getWeights(self, isTrain):
+        if isTrain:
+            # for training, returns the weights before eta/pt reweighting if available
+            if self.trainWeightsBeforePtEtaReweighting is None: 
+                return self.trainWeights
+            else:
+                return self.trainWeightsBeforePtEtaReweighting
+
+        else:
+            return self.testWeights
+
+    #----------------------------------------
+
+    def getLabels(self, isTrain):
+        if isTrain:
+            return self.trainLabels
+        else:
+            return self.testLabels
+
+    #----------------------------------------
+            
+    def hasTrainWeightsBeforePtEtaReweighting(self):
+        return self.trainWeightsBeforePtEtaReweighting != None
+
+
+#----------------------------------------------------------------------
+
 def addTimestamp(inputDir, x = 0.0, y = 1.07, ha = 'left', va = 'bottom'):
 
     import pylab, time
